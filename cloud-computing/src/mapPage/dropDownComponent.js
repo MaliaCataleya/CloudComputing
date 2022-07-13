cf4d4f10626975e4398a800b286ba004ab3eff9e
KaiKustermann <kai.kuster@gmx.de>
2022-07-13 14:57:23 +0200

    datepicker

--- cloud-computing/src/mapPage/dropDownComponent.js
+++ cloud-computing/src/mapPage/dropDownComponent.js
@@ -1,36 +1,30 @@
 import React from "react";
 import { Navbar, NavDropdown, Form, Button, Nav, FormControl, Offcanvas } from "react-bootstrap"
+import BootstrapDatePickerComponent from "./BootstrapDatePickerComponent";
 
 function DropDownComponent() {
   return (
-      <>
+    <>
       <div className="position-absolute top-0 start-0 w-25 bg-transparent">
-      <Navbar key={false} expand={false} className="px-3 bg-transparent">
-        <Navbar.Toggle aria-controls={`offcanvasNavbar`} />
-        <Navbar.Offcanvas id={`offcanvasNavbar`} aria-labelledby={`offcanvasNavbarLabel`} placement="start">
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id={`offcanvasNavbarLabel`}>Offcanvas</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id={`offcanvasNavbarDropdown`}>
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">Another Action</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form className="d-flex">
-              <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search"/>
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
+        <Navbar key={false} expand={false} className="px-3 bg-transparent">
+          <Navbar.Toggle aria-controls={`offcanvasNavbar`} />
+          <Navbar.Offcanvas id={`offcanvasNavbar`} aria-labelledby={`offcanvasNavbarLabel`} placement="start">
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel`}>Offcanvas</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <BootstrapDatePickerComponent></BootstrapDatePickerComponent>
+              </Nav>
+              <Form className="d-flex">
+                <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
         </Navbar>
-        </div>
-      </>
+      </div>
+    </>
   );
 }
 
