dd45f872274bc2d427919b465bdaff39757299d7
Mauff Sarah <sm236@hdm-stuttgart.de>
2022-07-13 15:09:26 +0200

    add radio buttons

--- cloud-computing/src/mapPage/dropDownComponent.js
+++ cloud-computing/src/mapPage/dropDownComponent.js
@@ -3,29 +3,32 @@ import { Navbar, NavDropdown, Form, Button, Nav, FormControl, Offcanvas } from "
 import BootstrapDatePickerComponent from "./BootstrapDatePickerComponent";
 
 function DropDownComponent() {
-  return (
-    <>
-      <div className="position-absolute top-0 start-0 w-25 bg-transparent">
-        <Navbar key={false} expand={false} className="px-3 bg-transparent">
-          <Navbar.Toggle aria-controls={`offcanvasNavbar`} />
-          <Navbar.Offcanvas id={`offcanvasNavbar`} aria-labelledby={`offcanvasNavbarLabel`} placement="start">
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id={`offcanvasNavbarLabel`}>Offcanvas</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <BootstrapDatePickerComponent></BootstrapDatePickerComponent>
-              </Nav>
-              <Form className="d-flex">
-                <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
-                <Button variant="outline-success">Search</Button>
-              </Form>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Navbar>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="position-absolute top-0 start-0 w-25 bg-transparent">
+                <Navbar key={false} expand={false} className="px-3 bg-transparent">
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar`} />
+                    <Navbar.Offcanvas id={`offcanvasNavbar`} aria-labelledby={`offcanvasNavbarLabel`} placement="start">
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id={`offcanvasNavbarLabel`}>Offcanvas</Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-end flex-grow-1 pe-3">
+                                <BootstrapDatePickerComponent></BootstrapDatePickerComponent>
+                            </Nav>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></input>
+                                <label class="form-check-label" for="flexRadioDefault1">Default radio</label></div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></input>
+                                <label class="form-check-label" for="flexRadioDefault1">Second default radio</label>
+                            </div>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Navbar>
+            </div>
+        </>
+    );
 }
 
 export default DropDownComponent;
