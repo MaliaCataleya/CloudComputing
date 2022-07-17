26f162e13305a9aa54d30edf0ccb3926540e6023
KaiKustermann <kai.kuster@gmx.de>
2022-07-17 22:09:04 +0200

    set location default checked

--- cloud-computing/src/mapPage/mapPageComponent.js
+++ cloud-computing/src/mapPage/mapPageComponent.js
@@ -6,22 +6,30 @@ import AppAPI from "../api/AppApi";
 
 function MapPageComponent() {
   const [date, setDate] = useState({});
-  const [locationChecked, setPlaceChecked] = useState(false);
+  const [locationChecked, setPlaceChecked] = useState(true);
   const [rangeChecked, setRangeChecked] = useState(false);
   const [routes, setRoutes] = useState(null);
   const [locations, setLocations] = useState(null);
 
-
   useEffect(() => {
     console.log(date, locationChecked, rangeChecked);
-    if(locationChecked === true){
-      var api = AppAPI.getAPI()
-      api.getLocationsByDateAndSessionId(date, sessionStorage.getItem("sessionId")).then((loc) => {setLocations(loc.docs)})
+    if (locationChecked === true) {
+      var api = AppAPI.getAPI();
+      api
+        .getLocationsByDateAndSessionId(
+          date,
+          sessionStorage.getItem("sessionId")
+        )
+        .then((loc) => {
+          if (loc.docs.length > 0) {
+            setLocations(loc.docs);
+          }
+        });
     }
   }, [date, locationChecked, rangeChecked]);
 
   useEffect(() => {
-    console.log(locations)
+    console.log(locations);
   }, [locations]);
 
   return (
