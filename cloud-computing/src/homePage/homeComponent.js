df1effb46bd5d4e49eed463f13e252754891d8bd
KaiKustermann <kai.kuster@gmx.de>
2022-08-12 18:41:26 +0200

    hint

--- cloud-computing/src/homePage/homeComponent.js
+++ cloud-computing/src/homePage/homeComponent.js
@@ -7,7 +7,7 @@ function HomeComponent() {
   const [uploaded, setUploaded] = useState(false);
   return (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-      <div>Download your google location data and upload the data of one month using the upload button. <a rel="noreferrer" target="_blank" href="https://support.google.com/accounts/answer/3024190?hl=en">Here</a> you can find a manual on how to download the data from your google account.</div>
+      <div>Download your google location data and upload the data of <b>one month</b> using the upload button. <a rel="noreferrer" target="_blank" href="https://support.google.com/accounts/answer/3024190?hl=en">Here</a> you can find a manual on how to download the data from your google account.</div>
       <UploadButton data-testid="uploadButton" setUploaded={setUploaded}></UploadButton>
       {uploaded ? (
         <Link to="/map">
